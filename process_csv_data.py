aeae9a6b8a4f1e4a88a5343bbe70fd081f54f172
weigego <weic6@outlook.com>
2024-03-14 16:29:44 -0500

    update draw script

--- process_csv_data.py
+++ process_csv_data.py
@@ -6,7 +6,7 @@ import os
 import json
 from glob import glob
 
-def combine_dataset(all_csv_files, out_csv_name):
+def combine_PUB_BID_dataset(all_csv_files, out_csv_name):
     """
     Filters rows, removes specific columns and then combines CSV files into a new CSV file.
 
@@ -22,10 +22,9 @@ def combine_dataset(all_csv_files, out_csv_name):
 
     for file in all_csv_files:
         df = pd.read_csv(file, low_memory=False)
-        filtered_df = df[(df['RESOURCE_TYPE'] == 'GENERATOR') & (df['MARKETPRODUCTTYPE'] == 'EN')]
+        filtered_df = df[(df['RESOURCE_TYPE'] == 'GENERATOR') & (df['MARKETPRODUCTTYPE'] == 'EN')] #consider generator and EN for now
         result_df = filtered_df.drop(columns=['STARTTIME_GMT', 'STOPTIME_GMT', 'STARTDATE', 'MARKET_RUN_ID', 'TIMEINTERVALSTART_GMT', 'TIMEINTERVALEND_GMT'])
         data_frames.append(result_df)
-        break
 
     combined_df = pd.concat(data_frames, ignore_index=True)
     sorted_df = combined_df.sort_values(by=['STARTTIME', 'STOPTIME'], ascending=True)
@@ -42,6 +41,29 @@ def combine_dataset(all_csv_files, out_csv_name):
     # dtype_df = pd.DataFrame(dtype_info)
     # dtype_df.to_csv('dtype_info.csv', index=False)  
 
+def combine_LMP_dataset(all_csv_files, out_csv_name):
+    """
+    Filters rows, removes specific columns and then combines CSV files into a new CSV file.
+
+    Parameters:
+    - all_csv_files (list): A list of filenames (str) of the CSV files to be processed.
+    - out_csv_name (str): The name of the output CSV file where the combined dataset will be saved.
+
+    Returns:
+    A new CSV file which contains the combined dataset.
+    """
+    
+    data_frames = [] 
+
+    for file in all_csv_files:
+        df = pd.read_csv(file, low_memory=False)
+        data_frames.append(df)
+
+    combined_df = pd.concat(data_frames, ignore_index=True)
+    sorted_df = combined_df.sort_values(by=['INTERVALSTARTTIME_GMT', 'INTERVALENDTIME_GMT'], ascending=True) ## TODO: match the date between LMP dataset and 
+    
+    return sorted_df.to_csv(f'{out_csv_name}.csv', index=False)
+
 def main():
     # load configuration
     with open('config.json', 'r') as config_file:
@@ -49,21 +71,42 @@ def main():
     train_percentage = config['train_percentage']
 
     # load the csv files
-    data_folder = os.path.join(os.path.dirname(__file__), '../../Data/DAM/PUB_BID')
-    csv_files = sorted(glob(os.path.join(data_folder, '*_PUB_BID_DAM_*.csv')))
+    pub_bid_data_folder = os.path.join(os.path.dirname(__file__), '../../Data/DAM/PUB_BID')
+    pub_bid_csv_files = sorted(glob(os.path.join(pub_bid_data_folder, '*_PUB_BID_DAM_*.csv')))
+    print(len(pub_bid_csv_files)) #87???
     
     # split dataset
-    split_ratio = int(len(csv_files) * (train_percentage / 100))
-    train_files = csv_files[:split_ratio]
-    valid_files = csv_files[split_ratio:]
-    # print(train_files[0], train_files[-1])
-    # print(valid_files[0], valid_files[-1])
+    split_ratio = int(len(pub_bid_csv_files) * (train_percentage / 100))
+    pub_bid_train_files = pub_bid_csv_files[:split_ratio]
+    pub_bid_valid_files = pub_bid_csv_files[split_ratio:]
+    # print(pub_bid_train_files[0], pub_bid_train_files[-1])
+    # print(pub_bid_valid_files[0], pub_bid_valid_files[-1])
     
-    output_train_csv_name = 'test1_train_combined'
-    output_valid_csv_name = 'test1_valid_combined'
+    output_pub_bid_train_csv_name = 'test1_pub_bid_train_combined'
+    output_pub_bid_valid_csv_name = 'test1_pub_bid_valid_combined'
 
-    combine_dataset(train_files, out_csv_name=output_train_csv_name)
-    combine_dataset(valid_files, out_csv_name=output_valid_csv_name)
+    # combine_PUB_BID_dataset(pub_bid_train_files, out_csv_name=output_pub_bid_train_csv_name)
+    # combine_PUB_BID_dataset(pub_bid_valid_files, out_csv_name=output_pub_bid_valid_csv_name)
+    
+    
+    
+    # load LMP csv files
+    lmp_data_folder = os.path.join(os.path.dirname(__file__), '../../Data/DAM/LMP')
+    lmp_csv_files = sorted(glob(os.path.join(lmp_data_folder, '*_LMP_DAM_LMP_*.csv')))
+    # print(lmp_csv_files)
+    
+    # split dataset
+    lmp_train_files = lmp_csv_files[:split_ratio]
+    lmp_valid_files = lmp_csv_files[split_ratio:]
+    # print(lmp_train_files[0], lmp_train_files[-1])
+    # print(lmp_valid_files[0], lmp_valid_files[-1])
+    # print(len(lmp_csv_files)) #93???
+    
+    #process LMP
+    output_lmp_train_csv_name = 'lmp_train_combined'
+    output_lmp_valid_csv_name = 'lmp_valid_combined'
+    combine_LMP_dataset(lmp_train_files, out_csv_name=output_lmp_train_csv_name)
+    combine_LMP_dataset(lmp_valid_files, out_csv_name=output_lmp_valid_csv_name)
     
 if __name__ == '__main__':
     main()
